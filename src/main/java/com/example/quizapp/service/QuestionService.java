ad3c261fc5e8de8f0c90b4ea0bb064e2a300e47d
razeemarc <razeemarc@gmail.com>
2024-05-29 22:46:47 +0530

    "database connected"

---
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.example.quizapp.service;
+
+import com.example.quizapp.dao.QuestionDao;
+import com.example.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+    public List<Question> getAllQuestions() {
+      return   questionDao.findAll();
+
+    }
+}
+
